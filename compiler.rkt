9dc3ac0bc55af90d7957531994754330f7f7a299
tkozek <tkozek@students.cs.ubc.ca>
2026-01-29 17:35:46 -0800

    Implemented normalize-bind and its helpers.

--- compiler.rkt
+++ compiler.rkt
@@ -142,10 +142,57 @@
         `(module ,(sequentialize-tail tail))])
     [_ (error "Expected values-unique-lang-v3, got: ~a" p)])  
 
-;; (imp-mf-lang-v3) -> (imp-cmf-lang-v3)
+;; (Imp-mf-lang-v3 effect) -> (Imp-cmf-lang-v3 effect)
+(define (normalize-effect effect)
+    (match effect
+    [`(set! ,aloc (begin ,effects ... ,value))
+        `(begin ,@(map normalize-effect effects)
+                (set! ,aloc ,(normalize-value value)))]
+    [`(set! ,aloc ,value)
+        `(set! ,aloc ,(normalize-value value))]
+    [`(begin ,effects ... ,effect2)
+        `(begin ,@(map normalize-effect effects)
+                    ,(normalize-effect effect2))]
+    [_ (error "Expected an effect, got: ~a" effect)]
+    
+    ))
+
+;; (Imp-mf-lang-v3 value) -> (Imp-cmf-lang-v3 value)
+(define (normalize-value value)
+    (match value
+    [(? triv?)
+        value]
+    [`(,op ,triv1 ,triv2)
+        (if (and (binop? op) (triv? triv1) (triv? triv2))
+            value
+            (error "Expected a value, got: ~a" value))]
+    [`(begin ,effects ... ,body)
+        `(begin ,@(map normalize-effect effects)
+                    ,(normalize-value body))]
+    [_ (error "Expected a value, got: ~a" value)]
+        
+        ))
+
+;; (Imp-mf-lang-v3 tail) -> (Imp-cmf-lang-v3 tail)
+(define (normalize-tail tail)
+    (match tail
+    [(? triv?)
+        (normalize-value tail)]
+    [`(,op ,triv1 ,triv2)
+        (normalize-value tail)]
+    [`(begin ,effects ... ,body)
+        `(begin ,@(map normalize-effect effects)
+                ,(normalize-tail body))]
+    [_ (error "Expected a tail, got: ~a" tail)]))
+
+
+;; (imp-mf-lang-v3 p) -> (imp-cmf-lang-v3 p)
 ;; Pushes 'set!' under 'begin' so that RHS of each 'set!' is a simple value producing operation
 (define (normalize-bind p)
-    p)
+    (match p
+    [`(module ,tail)
+        `(module ,(normalize-tail tail))]
+    [_ (error "Expected (module tail), got: ~a" p)]))
 
 
 ;; (imp-cmf-lang-v3) -> (asm-lang-v2)
