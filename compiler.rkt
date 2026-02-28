efab0f098ff9f65b93e7c382edc66f56261ac544
tkozek <tkozek@students.cs.ubc.ca>
2026-02-27 21:33:45 -0800

    Implemented wrap-x64- boilerplate and wrap-x64-run-time.

--- compiler.rkt
+++ compiler.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require cpsc411/compiler-lib)
+(require cpsc411/compiler-lib
+         cpsc411/langs/v1)
 
 (provide check-paren-x64
          interp-paren-x64
@@ -62,13 +63,13 @@
 
 ;; Optional; if you choose not to complete, implement a stub that returns a valid exit code
 (define (interp-paren-x64 p)
-  (define (eval-instruction-sequence regfile s)
-    (if (empty? s)
-        ; If no more instructions, return exit code modulo 256 (since operating
-        ; systems return exit code modulo 256).
-        (modulo (dict-ref regfile 'rax) 256)
-        0))
-  0)
+  ;   (define (eval-instruction-sequence regfile s)
+  ;     (if (empty? s)
+  ;         ; If no more instructions, return exit code modulo 256 (since operating
+  ;         ; systems return exit code modulo 256).
+  ;         (modulo (dict-ref regfile 'rax) 256)
+  ;         0))
+  (interp-paren-x64-v1 p))
 
 ;; (paren-x64-v1 p) -> x64-instruction-sequence
 ;; Compiles a Paren-x64 v1 program into x64 instruction sequence, represented as a string
@@ -96,94 +97,83 @@
 
 ;; Run-time system for paren-x64-v1
 ; (define run-time-exit "mov rdi, rax\nmov rax, 60")
-(define run-time-exit "mov rdi, rax\n")
-;; x64-instruction-sequence -> x64-instruction-sequence
+(define run-time-exit (format "exit:\nmov rdi, rax\nmov rax, ~a\n syscall" (sys-exit)))
+
+;; (x64-instruction-sequence )-> x64-instruction-sequence
 ;; Installs the paren-x64-v1 run-time system by appending it to input sequence
-(define (wrap-x64-run-time str)
-  (string-append str run-time-exit))
+(define (wrap-x64-run-time p)
+  (string-append p run-time-exit))
 
 ;; x64-instruction-sequence -> x64-instruction-sequence
 ;; Wraps x64-instruction-sequence with necessary boilerplate
 ;;  to return a complete x64 program in Intel syntax
-(define (wrap-x64-boilerplate str)
+(define (wrap-x64-boilerplate p)
   (define start
     #<<EOS
 global start
 
 section .text
 start:
-    mov rbx, 0
-
 EOS
     )
-  ; (define exit (format "exit:\nmov rax, ~a\nsyscall\n" (sys-exit 'unix))
-  ; (define exit "syscall\n")
-  (define exit
-    (string-append "exit:\nmov rax, "
-                   (number->string (sys-exit 'unix))
-                   "\nsyscall\nsection .data \n\ndummy db 0")
-    )
-
-  (~a start str exit #:separator "\n"))
+  (~a start p #:separator "\n"))
 
 (module+ test
-  (require rackunit
-           rackunit/text-ui
-           cpsc411/langs/v1
-           cpsc411/test-suite/public/v1)
+   (require rackunit
+                       rackunit/text-ui
+                       cpsc411/langs/v1
+                       cpsc411/test-suite/public/v1)
+    
+    (run-tests (v1-public-test-suite
+                (list check-paren-x64 generate-x64 wrap-x64-run-time wrap-x64-boilerplate)
+                (list interp-paren-x64-v1 interp-paren-x64-v1 #f #f)
+                check-paren-x64
+                interp-paren-x64))
+    
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax 0)))
+                  "mov rax, 0\n")
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax 0)
+                                   (set! rax (+ rax 42))))
+                  "mov rax, 0\nadd rax, 42\n")
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax ,(max-int 64))
+                                   (set! rdi ,(min-int 64))
+                                   (set! rdi rax)))
+                  "mov rax, 9223372036854775807\nmov rdi, -9223372036854775808\nmov rdi, rax\n")
 
-  (run-tests (v1-public-test-suite
-              (list check-paren-x64 generate-x64 wrap-x64-run-time wrap-x64-boilerplate)
-              (list interp-paren-x64-v1 interp-paren-x64-v1 #f #f)
-              check-paren-x64
-              interp-paren-x64)))
-(module+ test
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax 0)))
-                "mov rax, 0\n")
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax 0)
-                                 (set! rax (+ rax 42))))
-                "mov rax, 0\nadd rax, 42\n")
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax ,(max-int 64))
-                                 (set! rdi ,(min-int 64))
-                                 (set! rdi rax)))
-                "mov rax, 9223372036854775807\nmov rdi, -9223372036854775808\nmov rdi, rax\n")
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax ,(max-int 64))
+                                   (set! rdi ,(min-int 64))
+                                   (set! rdi (+ rdi rax))))
+                  "mov rax, 9223372036854775807\nmov rdi, -9223372036854775808\nadd rdi, rax\n")
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax 3)
+                                   (set! rdi 2)
+                                   (set! rdi (* rdi rax))))
+                  "mov rax, 3\nmov rdi, 2\nimul rdi, rax\n")
 
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax ,(max-int 64))
-                                 (set! rdi ,(min-int 64))
-                                 (set! rdi (+ rdi rax))))
-                "mov rax, 9223372036854775807\nmov rdi, -9223372036854775808\nadd rdi, rax\n")
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax 3)
-                                 (set! rdi 2)
-                                 (set! rdi (* rdi rax))))
-                "mov rax, 3\nmov rdi, 2\nimul rdi, rax\n")
-
-  (check-equal? (generate-x64 `(begin
-                                 (set! rax -1)
-                                 (set! rbx -1)
-                                 (set! rbx (+ rbx ,(max-int 32)))))
-                "mov rax, -1\nmov rbx, -1\nadd rbx, 2147483647\n")
-  (check-equal? (generate-x64 `(begin
-                                 (set! rcx -1)
-                                 (set! rax rcx)
-                                 (set! rcx (+ rcx ,(min-int 32)))))
-                "mov rcx, -1\nmov rax, rcx\nadd rcx, -2147483648\n")
-  (check-equal?
-   (generate-x64 `(begin
-                    (set! rdx 2)
-                    (set! rsi 3)
-                    (set! r11 4)
-                    (set! rsi (* rsi rdx))
-                    (set! r11 (+ r11 -1))
-                    (set! r12 (+ r12 r11))
-                    (set! rax r12)))
-   "mov rdx, 2\nmov rsi, 3\nmov r11, 4\nimul rsi, rdx\nadd r11, -1\nadd r12, r11\nmov rax, r12\n"))
-
-(module+ test
+    (check-equal? (generate-x64 `(begin
+                                   (set! rax -1)
+                                   (set! rbx -1)
+                                   (set! rbx (+ rbx ,(max-int 32)))))
+                  "mov rax, -1\nmov rbx, -1\nadd rbx, 2147483647\n")
+    (check-equal? (generate-x64 `(begin
+                                   (set! rcx -1)
+                                   (set! rax rcx)
+                                   (set! rcx (+ rcx ,(min-int 32)))))
+                  "mov rcx, -1\nmov rax, rcx\nadd rcx, -2147483648\n")
+    (check-equal?
+     (generate-x64 `(begin
+                      (set! rdx 2)
+                      (set! rsi 3)
+                      (set! r11 4)
+                      (set! rsi (* rsi rdx))
+                      (set! r11 (+ r11 -1))
+                      (set! r12 (+ r12 r11))
+                      (set! rax r12)))
+     "mov rdx, 2\nmov rsi, 3\nmov r11, 4\nimul rsi, rdx\nadd r11, -1\nadd r12, r11\nmov rax, r12\n")
 
   (check-equal? (wrap-x64-run-time (generate-x64 `(begin
                                                     (set! rax 0))))
@@ -197,7 +187,7 @@ EOS
                 (string-append (generate-x64 `(begin
                                                 (set! rax 0)
                                                 (set! rax (+ rax 42))))
-                               `run-time-exit))
+                               run-time-exit))
 
   (check-equal? (wrap-x64-run-time (generate-x64 `(begin
                                                     (set! rax ,(max-int 64))
@@ -266,6 +256,3 @@ EOS
                                                 (set! r12 (+ r12 r11))
                                                 (set! rax r12)))
                                run-time-exit)))
-
-(module+ test
-  )
