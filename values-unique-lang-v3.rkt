0aede19946104827b364ed4b596e8edfe474687c
tkozek <tkozek@students.cs.ubc.ca>
2026-03-08 20:26:22 -0700

    Added more extensive tests for normalize-bind. Fixed syntax rule definition in values-unique-lang

--- values-unique-lang-v3.rkt
+++ values-unique-lang-v3.rkt
@@ -36,7 +36,7 @@
   (require rackunit
            cpsc411/langs/v3)
   (define-syntax-rule (check-by-interp p)
-    (check-equal? (interp-values-unique-lang-v3 p) (interp-imp-mf-lang-v3 (uniquify p))))
+    (check-equal? (interp-values-unique-lang-v3 p) (interp-imp-mf-lang-v3 (sequentialize-let p))))
 
   ;; Added March 8th, 2026
   (check-by-interp '(module 0))
