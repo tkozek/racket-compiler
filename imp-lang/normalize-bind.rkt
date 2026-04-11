827c0f25d8a40599539aeed8d39a0ec13b33d951
tkozek <tkozek@students.cs.ubc.ca>
2026-04-11 12:59:47 -0700

    Fixed normalize-bind, now passes aloc into value helper

--- imp-lang/normalize-bind.rkt
+++ imp-lang/normalize-bind.rkt
@@ -10,36 +10,27 @@
 ;; Pushes 'set!' under 'begin' so that RHS of each 'set!' is a simple value producing operation
 (define (normalize-bind p)
 
-  ;; (Imp-mf-lang-v3 effect) -> (Imp-cmf-lang-v3 effect)
+  ;; (imp-mf-lang-v3 effect) -> (imp-cmf-lang-v3 effect)
   (define (normalize-effect effect)
     (match effect
-      [`(set! ,aloc ,value)
-       (let ([normalized-val (normalize-value value)])
-         (match normalized-val
-           [`(begin
-               ,effects ...
-               ,body)
-            `(begin
-               ,@effects
-               (set! ,aloc ,body))]
-           [_ `(set! ,aloc ,normalized-val)]))]
+      [`(set! ,aloc ,value) (normalize-value value aloc)]
       [`(begin
           ,effects ...)
        `(begin
           ,@(map normalize-effect effects))]))
 
   ;; (imp-mf-lang-v3 value) -> (imp-cmf-lang-v3 value)
-  (define (normalize-value value)
+  (define (normalize-value value aloc)
     (match value
       [`(begin
           ,effects ...
           ,body)
        `(begin
           ,@(map normalize-effect effects)
-          ,(normalize-value body))]
-      [_ value]))
+          ,(normalize-value body aloc))]
+      [_ `(set! ,aloc ,value)]))
 
-  ;; (Imp-mf-lang-v3 tail) -> (Imp-cmf-lang-v3 tail)
+  ;; (imp-mf-lang-v3 tail) -> (imp-cmf-lang-v3 tail)
   (define (normalize-tail tail)
     (match tail
       [`(begin
@@ -48,16 +39,10 @@
        `(begin
           ,@(map normalize-effect effects)
           ,(normalize-tail body))]
-      [_ (normalize-value tail)]))
+      [_ tail]))
 
   (define (normalize-p p)
     (match p
       [`(module ,tail) `(module ,(normalize-tail tail))]))
 
   (normalize-p p))
-
-(module+ test
-  (require rackunit
-           cpsc411/langs/v3)
-  (define-syntax-rule (check-by-interp p)
-    (check-equal? (interp-imp-mf-lang-v3 p) (interp-imp-cmf-lang-v3 (normalize-bind p)))))
