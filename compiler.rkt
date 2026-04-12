6b375c3ded22b9ff9c370c14ef1b71126c30e867
UBC Student <hpowel03@pender.students.cs.ubc.ca>
2026-04-12 14:44:48 -0700

    removed m8 stubs

--- compiler.rkt
+++ compiler.rkt
@@ -81,8 +81,7 @@
 
 
 ;; Stubs; remove or replace with your definitions.
-(define-values (uniquify
-                expand-macros
+(define-values (expand-macros
                 implement-safe-primops
                 implement-safe-call
                 define->letrec
@@ -95,49 +94,8 @@
                 implement-closures
                 specify-representation
                 remove-complex-opera*
-                sequentialize-let
-                normalize-bind
-                impose-calling-conventions
-                select-instructions
-                expose-allocation-pointer
-                uncover-locals
-                undead-analysis
-                conflict-analysis
-                assign-call-undead-variables
-                allocate-frames
-                assign-registers
-                assign-frame-variables
-                replace-locations
-                implement-fvars
-                optimize-predicates
-                expose-basic-blocks
-                resolve-predicates
-                flatten-program
-                patch-instructions
-                implement-mops
-                generate-x64)
+                implement-mops)
   (values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
-   values
    values
    values
    values
